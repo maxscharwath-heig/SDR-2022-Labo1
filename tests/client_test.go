12d11e47730137a37d0220fd17fa7af64ed8b881
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-10-02 14:17:49 +0200

    Init tests

---
+++ tests/client_test.go
@@ -0,0 +1,9 @@
+package tests
+
+import "testing"
+
+// Si jamais les test doivent toujours être nommés: TestXXXXX
+
+func TestClient(t *testing.T) {
+
+}
---
+++ tests/server_test.go
@@ -0,0 +1,7 @@
+package tests
+
+import "testing"
+
+func TestServer(t *testing.T) {
+
+}
