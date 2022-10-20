952d225f2444892c2c439bf5a73ccf79f21c71b4
Maxime Scharwath <maxscharwath@gmail.com>
2022-10-20 09:35:14 +0200

    add levenstein LOL

--- client.go
+++ client.go
@@ -13,6 +13,7 @@ import (
 	"sdr/labo1/src/types"
 	"sdr/labo1/src/utils"
 	"sdr/labo1/src/utils/colors"
+	"sdr/labo1/src/utils/levenstein"
 	"strconv"
 	"strings"
 	"syscall"
@@ -205,7 +206,10 @@ func clientProcess(configuration config.ClientConfiguration) {
 			disconnect(conn)
 			return
 		default:
-			PrintError("Invalid command, try again")
+			couldBe := levenstein.SearchBestMatch(cmd, []string{
+				"create", "close", "register", "show", "quit",
+			})
+			PrintError(fmt.Sprintf("Unknown command \"%s\", did you mean \"%s\"?", cmd, couldBe))
 		}
 	}
 }
---
+++ src/utils/levenstein/levenstein.go
@@ -0,0 +1,77 @@
+package levenstein
+
+import "unicode/utf8"
+
+const minLengthThreshold = 32
+
+// source: https://github.com/agnivade/levenshtein
+
+func ComputeDistance(a, b string) int {
+	if len(a) == 0 {
+		return utf8.RuneCountInString(b)
+	}
+
+	if len(b) == 0 {
+		return utf8.RuneCountInString(a)
+	}
+
+	if a == b {
+		return 0
+	}
+
+	s1 := []rune(a)
+	s2 := []rune(b)
+
+	if len(s1) > len(s2) {
+		s1, s2 = s2, s1
+	}
+	lenS1 := len(s1)
+	lenS2 := len(s2)
+
+	var x []uint16
+	if lenS1+1 > minLengthThreshold {
+		x = make([]uint16, lenS1+1)
+	} else {
+		x = make([]uint16, minLengthThreshold)
+		x = x[:lenS1+1]
+	}
+
+	for i := 1; i < len(x); i++ {
+		x[i] = uint16(i)
+	}
+
+	_ = x[lenS1]
+	for i := 1; i <= lenS2; i++ {
+		prev := uint16(i)
+		for j := 1; j <= lenS1; j++ {
+			current := x[j-1] // match
+			if s2[i-1] != s1[j-1] {
+				current = min(min(x[j-1]+1, prev+1), x[j]+1)
+			}
+			x[j-1] = prev
+			prev = current
+		}
+		x[lenS1] = prev
+	}
+	return int(x[lenS1])
+}
+
+func min(a, b uint16) uint16 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func SearchBestMatch(a string, b []string) string {
+	var bestMatch string
+	var bestDistance int
+	for _, bItem := range b {
+		distance := ComputeDistance(a, bItem)
+		if bestMatch == "" || distance < bestDistance {
+			bestMatch = bItem
+			bestDistance = distance
+		}
+	}
+	return bestMatch
+}
