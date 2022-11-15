0b8db20c49c6918c4b2c9241ef82ce95a7c92098
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-15 19:25:34 +0100

    Lamport definition

--- src/lamport/lamport.go
+++ src/lamport/lamport.go
@@ -29,11 +29,66 @@ type Lamport struct {
 	states    map[string]Request
 }
 
+// InitLamport inits the needed structure for lamport's algorithm
+func InitLamport(numberOfServers int, id string) Lamport {
+	var lmp = Lamport{
+		stamp:     0,
+		id:        id,
+		hasAccess: false,
+		states:    make(map[string]Request, numberOfServers),
+	}
+
+	// Init all to REL, stamp 0
+	for key := range lmp.states {
+		lmp.states[key] = Request{
+			ReqType:  REL,
+			Stamp:    lmp.stamp,
+			Global:   false,
+			Sender:   lmp.id,
+			Receiver: lmp.id,
+		}
+	}
+
+	return lmp
+}
+
+func (l *Lamport) setCurrentState(request Request) {
+	l.states[l.id] = request
+}
+
 func (l *Lamport) currentState() Request {
 	return l.states[l.id]
 }
 
-func (l *Lamport) handleLamportRequest(req Request) {
+// TODO: on peut fusionner les deux functions car au final elle font la même chose (REQ, REL)
+
+// HandleClientAskCriticalSection
+func (l *Lamport) HandleClientAskCriticalSection() {
+	l.stamp += 1
+	l.setCurrentState(Request{
+		ReqType: REQ,
+		Stamp:   l.stamp,
+		Global:  true,
+		Sender:  l.id,
+	})
+
+	// TODO: Envoi de la requête (currentState - REQ) vers tous (broadcast)
+}
+
+func (l *Lamport) HandleClientReleaseCriticalSection() {
+	l.stamp += 1
+	l.setCurrentState(Request{
+		ReqType: REL,
+		Stamp:   l.stamp,
+		Global:  true,
+		Sender:  l.id,
+	})
+
+	// TODO: Envoi de la requête (currentState - REL) vers tous (broadcast)
+}
+
+// HandleLamportRequest Traiment des messages entre serveurs
+func (l *Lamport) HandleLamportRequest(req Request) {
 	l.stamp = int(math.Max(float64(l.stamp), float64(req.Stamp)) + 1)
 
 	switch req.ReqType {
@@ -64,22 +119,28 @@ func (l *Lamport) handleLamportRequest(req Request) {
 	l.checkCriticalSectionAccess()
 }
 
+// checkCriticalSectionAccess check if can access to SC and set access if so
 func (l *Lamport) checkCriticalSectionAccess() {
 	if l.currentState().ReqType != REQ {
 		return
 	}
 	oldest := true
 
-	/*
-		plusAncienne := vrai
-		pour chaque i entre 0 et N sauf moi
-			si t[moi].estampille > t[i].estampille alors
-				plusAncienne := faux; sortir du pour
-			sinon si t[moi].estampille = t[i].estampille et moi > i alors
-				plusAncienne := faux; sortir du pour
-			fin si
-		fin pour
-	*/
+	for i := range l.states {
+		if i == l.id {
+			continue
+		}
+
+		if l.currentState().Stamp > l.states[i].Stamp {
+			oldest = false
+			break
+		}
+
+		if l.currentState().Stamp == l.states[i].Stamp && l.id > i {
+			oldest = false
+			break
+		}
+	}
 
 	if oldest {
 		l.hasAccess = true
